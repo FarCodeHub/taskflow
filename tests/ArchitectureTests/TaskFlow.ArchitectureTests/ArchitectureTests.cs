33990587a1a9c86f8d4a894ef320a34b61db4011
farhad_salehi_21 <farhad_salehi_21@outlook.com>
2026-05-06 10:45:55 +0330

    test(architecture): enforce clean architecture rules

---
+++ tests/ArchitectureTests/TaskFlow.ArchitectureTests/ArchitectureTests.cs
@@ -0,0 +1,41 @@
+﻿using NetArchTest.Rules;
+
+namespace TaskFlow.ArchitectureTests;
+
+public class ArchitectureTests
+{
+    [Fact]
+    public void Domain_Should_Not_Depend_On_Other_Projects()
+    {
+        var result = Types.InAssembly(typeof(TaskFlow.Tasks.Domain.AssemblyReference).Assembly)
+            .ShouldNot()
+            .HaveDependencyOnAny(
+                "TaskFlow.Tasks.Application",
+                "TaskFlow.Tasks.Infrastructure",
+                "TaskFlow.Tasks.Api"
+            )
+            .GetResult();
+
+        Assert.True(result.IsSuccessful);
+    }
+
+    [Fact]
+    public void Application_Should_Not_Depend_On_Infrastructure()
+    {
+        var result = Types.InAssembly(typeof(TaskFlow.Tasks.Application.AssemblyReference).Assembly).ShouldNot().HaveDependencyOn("TaskFlow.Tasks.Infrastructure").GetResult();
+        Assert.True(result.IsSuccessful);
+    }
+
+
+    [Fact]
+    public void Api_Should_Not_Depend_On_Domain_Directly()
+    {
+        var result = Types.InAssembly(typeof(TaskFlow.Tasks.Api.AssemblyReference).Assembly)
+            .ShouldNot()
+            .HaveDependencyOn("TaskFlow.Tasks.Domain")
+            .GetResult();
+
+        Assert.True(result.IsSuccessful);
+    }
+
+}
